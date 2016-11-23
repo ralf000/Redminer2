850a8faec52ac70cb8b3ffc880006aa4e9e569ff
ralf <ralf000@yandex.ru>
2016-11-23 12:58:22 +0300

    added docs for lib.js

--- lib.js
+++ lib.js
@@ -7,6 +7,10 @@ function ucFirst(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+/**
+ * Возращает id для активного окна, вычисляемый по активной вкладке
+ * @returns string id 
+ */
 function getActiveTabIdByHPSM() {
     var tabActive = $('ul.x-tab-strip-top li.x-tab-strip-active');
     if (tabActive.length === 0)
@@ -16,16 +20,28 @@ function getActiveTabIdByHPSM() {
     return id[id.length-1];
 }
 
+/**
+ * Возвращает активную область
+ * @returns object jquery active area
+ */
 function getActiveWindowByHPSM() {
     var tabId = getActiveTabIdByHPSM();
     return (tabId) ? $('.x-tab-panel-bwrap').find('#'+tabId) : false;
 }
 
+/**
+ * Возвращает активный iframe
+ * @returns object jquery iframe
+ */
 function getActiveFrameByHPSM() {
     var w = getActiveWindowByHPSM();
     return (w) ? w.find('iframe').contents() : false;
 }
 
+/**
+ * Возвращает активную форму
+ * @returns object jquery active form
+ */
 function getActiveFormByHPSM() {
     var frame = getActiveFrameByHPSM();
     return (frame) ? frame.find('form#topaz') : false;
