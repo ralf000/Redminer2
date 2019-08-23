1de5a012b827e8c6cef392d299f8c0d88ad5a528
Kudinov <kudinov@upt24.ru>
2019-08-23 15:42:57 +0300

    bugfix

--- onUpdate.js
+++ onUpdate.js
@@ -7,7 +7,7 @@ function showRedmineUrl() {
         }
 
         if ($('body a#redmine'))
-            $('body a#redmine').empty();
+            $('body a#redmine').remove();
 
         var form = getActiveFormByHPSM();//lib.js
 
@@ -15,7 +15,7 @@ function showRedmineUrl() {
             var url = form.find('input[name="instance/hpc.additional.field.2"]').val();
             if (url) {
                 $('body')
-                    .append('<a id="redmine" target="_blank" href="' + url + '"><button style="position: fixed; bottom: 0; left: 0; width: 60px; height: 30px; background-color: #0096D6; color: white; font-size: 10px;">Redmine</button></a>');
+                    .append('<a id="redmine" target="_blank" href="https://rmine.net/issues/' + url + '"><button style="position: fixed; bottom: 0; left: 0; width: 60px; height: 30px; background-color: #0096D6; color: white; font-size: 10px;">Redmine</button></a>');
             }
             clearTimeout(timeout);
             showRedmineUrl();
